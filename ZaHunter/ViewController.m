b42f8cc138a9261d6aa7581380937c46f24e01a8
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-15 16:21:31 -0500

    task #1 complete
    List of pizza restaurants

--- ZaHunter/ViewController.m
+++ ZaHunter/ViewController.m
@@ -7,21 +7,91 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
 
-@interface ViewController ()
 
+@interface ViewController () <CLLocationManagerDelegate, UITableViewDataSource, UITableViewDelegate>
+@property CLLocationManager *myLocationManager;
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property NSArray *pizzaArray;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.myLocationManager = [[CLLocationManager alloc] init];
+    [self.myLocationManager requestWhenInUseAuthorization];
+    self.myLocationManager.delegate = self;
+
+    [self.myLocationManager startUpdatingLocation];
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.pizzaArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellID" forIndexPath:indexPath];
+
+    MKMapItem *item = [self.pizzaArray objectAtIndex:indexPath.row];
+    cell.textLabel.text = [item name];
+
+    return cell;
+}
+
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations{
+    for (CLLocation *location in locations) {
+        if(location.verticalAccuracy < 1000 && location.horizontalAccuracy < 1000){
+            [self reverseGeocode:location];
+            NSLog(@"location: %@",location);
+            [self.myLocationManager stopUpdatingLocation];
+            break;
+        }
+    }
+}
+
+- (void)reverseGeocode:(CLLocation *)location{
+    CLGeocoder *geocoder = [CLGeocoder new];
+    [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
+        CLPlacemark *placemark = placemarks.firstObject;
+        NSString *address = [NSString stringWithFormat:@"%@ %@ \n%@",placemark.subThoroughfare,placemark.thoroughfare,placemark.locality];
+        NSLog(@"Found you: %@",address);
+        [self findPizzaNear:placemark.location];
+    }];
+}
+
+- (void)findPizzaNear:(CLLocation *)location{
+    MKLocalSearchRequest *request = [MKLocalSearchRequest new];
+    request.naturalLanguageQuery = @"pizza";
+    request.region = MKCoordinateRegionMake(location.coordinate, MKCoordinateSpanMake(1, 1));
+    MKLocalSearch *search = [[MKLocalSearch alloc] initWithRequest:request];
+    [search startWithCompletionHandler:^(MKLocalSearchResponse *response, NSError *error) {
+        self.pizzaArray = response.mapItems;
+        [self.tableView reloadData];
+    }];
+}
+
+
+
+
+
+
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+    UIAlertView *errorAlert = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                         message:@"Failed to Get Your Location"
+                                                        delegate:nil
+                                               cancelButtonTitle:@"OK"
+                                               otherButtonTitles:nil];
+    [errorAlert show];
+    NSLog(@"Error: %@",error);
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
