15045b438a39e51ecb93222124b7b428c25bd37f
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-15 14:30:31 -0500

    Initial Commit

---
+++ ZaHunter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZaHunter
+//
+//  Created by Eduardo Alvarado Díaz on 10/15/14.
+//  Copyright (c) 2014 Organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
